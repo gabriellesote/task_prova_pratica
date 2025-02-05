73c4a9b452ec34ba4a55443bd796a50391abb5c1
gabriellesote <gabrielle.sote@gmail.com>
2025-02-05 14:50:41 -0300

    criação de controllers de edição,criação,listagem

---
+++ sistema/backend/controllers/produtosController.js
@@ -0,0 +1,54 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export const criarProduto =  async (req,res) =>{
+
+  const  {nome_produto, quantidade, valor } = req.body
+  try{
+      const novoProduto = await prisma.produtos.create({
+        data: {
+          nome_produto,
+          quantidade,
+          valor
+        }
+      })
+
+      res.status(201).json(novoProduto)
+  }
+  catch(error) {
+    res.status(500).json({error: `Erro ao criar produto ${nome_produto}` + error.message})
+  }
+
+}
+
+export const editarProduto = async (req,res) => {
+  const {id} = req.params;
+  const {nome_produto,valor,quantidade} = req.body;
+
+  try{
+    const produtoAtualizado = await prisma.produtos.update({
+      where: {id:parseInt(id)},
+      data: {
+        nome_produto,
+        valor,
+        quantidade,
+        modified_at: new Date() 
+      }
+    })
+
+    res.json(produtoAtualizado)
+  } catch (error){
+    res.status(500).json({error: `Erro ao atualizar o produto ${id}`})
+  }
+}
+
+export const listarProdutos = async (req,res) => {
+  try{
+    const listar = await prisma.produtos.findMany();
+    res.status(200).json(listar)
+  }
+  catch(error){
+    res.status(400).json({message: 'Erro ao listar produtos'}, message.error)
+  }
+}
