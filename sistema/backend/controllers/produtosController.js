190c19bd52623f971a0dea3050099800d199c13e
gabriellesote <gabrielle.sote@gmail.com>
2025-02-05 15:13:23 -0300

    rota de delete criada

--- sistema/backend/controllers/produtosController.js
+++ sistema/backend/controllers/produtosController.js
@@ -2,66 +2,67 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-export const criarProduto =  async (req,res) =>{
+export const criarProduto = async (req, res) => {
+  const { nome_produto, quantidade, valor } = req.body;
+  try {
+    const novoProduto = await prisma.produtos.create({
+      data: {
+        nome_produto,
+        quantidade,
+        valor,
+      },
+    });
 
-  const  {nome_produto, quantidade, valor } = req.body
-  try{
-      const novoProduto = await prisma.produtos.create({
-        data: {
-          nome_produto,
-          quantidade,
-          valor
-        }
-      })
-
-      res.status(201).json(novoProduto)
+    res.status(201).json(novoProduto);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: `Erro ao criar produto ${nome_produto}` + error.message });
   }
-  catch(error) {
-    res.status(500).json({error: `Erro ao criar produto ${nome_produto}` + error.message})
-  }
+};
 
-}
+export const editarProduto = async (req, res) => {
+  const { id } = req.params;
+  const { nome_produto, valor, quantidade } = req.body;
 
-export const editarProduto = async (req,res) => {
-  const {id} = req.params;
-  const {nome_produto,valor,quantidade} = req.body;
-
-  try{
+  try {
     const produtoAtualizado = await prisma.produtos.update({
-      where: {id:parseInt(id)},
+      where: { id: parseInt(id) },
       data: {
         nome_produto,
         valor,
         quantidade,
-        modified_at: new Date() 
-      }
-    })
+        modified_at: new Date(),
+      },
+    });
 
-    res.json(produtoAtualizado)
-  } catch (error){
-    res.status(500).json({error: `Erro ao atualizar o produto ${id}`})
+    res.json(produtoAtualizado);
+  } catch (error) {
+    res.status(500).json({ error: `Erro ao atualizar o produto ${id}` });
   }
-}
+};
 
-export const listarProdutos = async (req,res) => {
-  try{
+export const listarProdutos = async (req, res) => {
+  try {
     const listar = await prisma.produtos.findMany();
-    res.status(200).json(listar)
+    res.status(200).json(listar);
+  } catch (error) {
+    res.status(400).json({ message: "Erro ao listar produtos" }, message.error);
   }
-  catch(error){
-    res.status(400).json({message: 'Erro ao listar produtos'}, message.error)
-  }
-}
+};
 
-export const deletarProduto = async(req,res) => {
-  const {id} = req.params.id
-  try{
+export const deletarProduto = async (req, res) => {
+  const { id } = req.params;
+  try {
     const deletar = await prisma.produtos.delete({
-      where: {id}
-    })
-    res.status(200).json({message: 'Produto deletado com sucesso!'})
+      where: { id: parseInt(id) },
+    });
+    res
+      .status(200)
+      .json({ message: "Produto deletado com sucesso!" });
+  } catch (error) {
+    res
+      .status(400)
+      .json({ message: "Erro ao deletar produto!" } + error.message);
   }
-  catch(error){
-    res.status(400).json({message: 'Erro ao deletar produto!'}, error.message)
-  }
-}
+};
--- sistema/backend/routes/produtosRoutes.js
+++ sistema/backend/routes/produtosRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { criarProduto, editarProduto, listarProdutos } from "../controllers/produtosController.js";
+import { criarProduto, editarProduto, listarProdutos, deletarProduto} from "../controllers/produtosController.js";
 const router = express.Router();
 router.post("/criar", criarProduto)
 router.put("/editar/:id", editarProduto)
 router.get("/listar", listarProdutos)
+router.delete("/deletar/:id", deletarProduto)
 
 
 
