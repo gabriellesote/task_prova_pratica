3cac2f00637c889b65c71258747bdcbbe9e0904c
gabriellesote <gabrielle.sote@gmail.com>
2025-02-06 11:15:22 -0300

    criando a logica do front

---
+++ sistema/frontend/scripts/modulos/criarProduto.js
@@ -0,0 +1,47 @@
+const nome = document.getElementById("nome")
+const quantidade = document.getElementById("quantidade")
+const valor = document.getElementById("valor")
+
+export async function adicionarProduto(event) {
+  event.preventDefault();
+
+  const nomeProduto = nome.value.trim();
+  const quantidadeProduto = quantidade.value.trim();
+  const valorProduto = parseFloat(valor.value);
+
+  console.log(nomeProduto, quantidadeProduto, valorProduto);
+
+  if (!nomeProduto || !quantidadeProduto || isNaN(valorProduto)) {
+    alert("Preencha todos os campos!");
+    return;
+  }
+
+  if (!Number.isInteger(Number(quantidadeProduto))) {
+    alert("A quantidade deve ser um número inteiro!");
+    return;
+  }
+
+  try {
+    const response = await axios.post("http://localhost:2002/produtos/criar", {
+      nome_produto: nomeProduto,
+      quantidade: parseInt(quantidadeProduto),
+      valor: valorProduto,
+    });
+
+    console.log("Sucesso:", response.data);
+    alert("Produto adicionado com sucesso!");
+
+    nome.value = ""
+    quantidade.value = ""
+    valor.value = ""
+
+  } catch (error) {
+    console.error("Um erro ocorreu:", error);
+    alert("Erro ao adicionar o produto!");
+  }
+
+
+
+}
+
+document.querySelector(".formulario").addEventListener("submit", adicionarProduto);
---
+++ sistema/frontend/scripts/script.js
@@ -0,0 +1,4 @@
+import { adicionarProduto } from "./modulos/criarProduto.js"
+export const apiURL = "http://localhost:2002/produtos/"
+
+adicionarProduto()
